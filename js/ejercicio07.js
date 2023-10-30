502862b3d64dc54e30f9f1c70d562bb3c73af263
Caro Valverde <carolina.valverde255@gmail.com>
2023-10-30 19:25:29 -0300

    agrego ejercicio 7 corregido

--- js/ejercicio07.js
+++ js/ejercicio07.js
@@ -1,16 +1,20 @@
-let valor1 = parseInt(prompt('Ingrese un número'));
-let valor2 = parseInt(prompt('Ingrese otro número'));
-let valor3 = parseInt(prompt('Ingrese otro número'));
+const valor1 = parseInt(prompt('Ingrese un número'));
+const valor2 = parseInt(prompt('Ingrese otro número'));
+const valor3 = parseInt(prompt('Ingrese otro número'));
+let mayor;
 
-var mayor = valor1;
-
-if(valor2 > mayor) {
+if(valor1 > valor2) {
+    mayor = valor1;
+}
+if(valor2 > valor1) {
     mayor = valor2;
 }
-
-if(valor3 > mayor) {
-    mayor = valor3;
+if(mayor > valor3) {
+document.write("El número más grande es el " + mayor); 
+}else{
+    document.write("El número más grande es el " + valor3);
 }
 
-document.write("El número más grande es el " + mayor);
+
+
 
